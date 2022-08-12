bec41de39a92f6c05ca9f57dd726cfb97f451aaa
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-08-12 14:42:11 -0300

    Add files via upload
    First App

---
+++ iOS/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Livia Carvalho Keller on 11/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
